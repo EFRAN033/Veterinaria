24b24d8f297298dcf5c5db96b70e951daacd8d73
EFRAN033 <efran.quispe13@gmail.com>
2025-11-25 07:19:51 -0500

    Move create_test_users.py to backend root

--- back_veterinaria/create_test_users.py
+++ back_veterinaria/create_test_users.py
@@ -3,9 +3,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Add the parent directory to sys.path to allow imports from app
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from app.core.config import settings
 from app.infrastructure.database.models.user import User
 from app.core.security import get_password_hash
